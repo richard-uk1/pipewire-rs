059e12258aab580cfcf8e35a0526653d32469e1e
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2020-06-24 11:43:32 +0200

    add pipewire crate
    Will be used to implement the high level safe API.

---
+++ pipewire/README.md
@@ -0,0 +1,10 @@
+# pipewire [![](https://img.shields.io/crates/v/pipewire.svg)](https://crates.io/crates/pipewire) [![](https://docs.rs/pipewire/badge.svg)](https://docs.rs/pipewire)
+
+[PipeWire](https://pipewire.org) bindings for Rust.
+
+These bindings are providing a safe API that can be used to interface with
+[PipeWire](https://pipewire.org).
+
+## Documentation
+
+See the [crate documentation](https://docs.rs/pipewire/).
---
+++ pipewire/src/lib.rs
@@ -0,0 +1,33 @@
+use std::ptr;
+
+use pipewire_sys as pw_sys;
+
+/// Initialize PipeWire
+///
+/// Initialize the PipeWire system and set up debugging
+/// through the environment variable `PIPEWIRE_DEBUG`.
+pub fn init() {
+    unsafe { pw_sys::pw_init(ptr::null_mut(), ptr::null_mut()) }
+}
+
+/// Deinitialize PipeWire
+///
+/// # Safety
+/// This must only be called once during the lifetime of the process, once no PipeWire threads
+/// are running anymore and all PipeWire resources are released.
+pub unsafe fn deinit() {
+    pw_sys::pw_deinit()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_init() {
+        init();
+        unsafe {
+            deinit();
+        }
+    }
+}
