4305c288933642203b38f1157c724c8cfd0ff15d
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2020-11-12 11:44:37 +0100

    main_loop: wrap around a Rc
    This will allow us to keep the loop alive while other object such are
    Context are around.

--- pipewire/src/main_loop.rs
+++ pipewire/src/main_loop.rs
@@ -2,15 +2,45 @@
 // SPDX-License-Identifier: MIT
 
 use pipewire_sys as pw_sys;
+use std::ops::Deref;
 use std::ptr;
+use std::rc::Rc;
 
 use crate::error::Error;
 use crate::loop_::Loop;
 
 #[derive(Debug)]
-pub struct MainLoop(*mut pw_sys::pw_main_loop);
+pub struct MainLoop {
+    inner: Rc<MainLoopInner>,
+}
 
 impl MainLoop {
+    pub fn new() -> Result<Self, Error> {
+        let inner = MainLoopInner::new()?;
+        Ok(Self {
+            inner: Rc::new(inner),
+        })
+    }
+}
+
+impl Deref for MainLoop {
+    type Target = MainLoopInner;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl Loop for MainLoop {
+    fn as_ptr(&self) -> *mut pw_sys::pw_loop {
+        unsafe { pw_sys::pw_main_loop_get_loop(self.inner.0) }
+    }
+}
+
+#[derive(Debug)]
+pub struct MainLoopInner(*mut pw_sys::pw_main_loop);
+
+impl MainLoopInner {
     // TODO: props argument
     pub fn new() -> Result<Self, Error> {
         unsafe {
@@ -18,7 +48,7 @@ impl MainLoop {
             if l.is_null() {
                 Err(Error::CreationFailed)
             } else {
-                Ok(MainLoop(l))
+                Ok(MainLoopInner(l))
             }
         }
     }
@@ -36,13 +66,7 @@ impl MainLoop {
     }
 }
 
-impl Loop for MainLoop {
-    fn as_ptr(&self) -> *mut pw_sys::pw_loop {
-        unsafe { pw_sys::pw_main_loop_get_loop(self.0) }
-    }
-}
-
-impl Drop for MainLoop {
+impl Drop for MainLoopInner {
     fn drop(&mut self) {
         unsafe { pw_sys::pw_main_loop_destroy(self.0) }
     }
