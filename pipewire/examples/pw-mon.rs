3b0ee18ba062f58018e19a6ada4395f15ba69087
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2021-01-20 13:53:59 +0100

    pw-mon: use HashMap to store proxies and listeners
    Will allow us to easily drop the proxies and listeners when proxies are
    removed.
    
    Also move the proxies and listeners to their own struct to make the code
    simpler.

--- pipewire/examples/pw-mon.rs
+++ pipewire/examples/pw-mon.rs
@@ -4,8 +4,8 @@
 use anyhow::Result;
 use pipewire as pw;
 use signal::Signal;
-use std::cell::RefCell;
 use std::sync::Arc;
+use std::{cell::RefCell, collections::HashMap};
 use structopt::StructOpt;
 
 use pw::node::Node;
@@ -14,6 +14,30 @@ use pw::prelude::*;
 use pw::properties;
 use pw::proxy::{Listener, ProxyT};
 use pw::registry::ObjectType;
+
+struct Proxies {
+    proxies_t: HashMap<u32, Box<dyn ProxyT>>,
+    listeners: HashMap<u32, Box<dyn Listener>>,
+}
+
+impl Proxies {
+    fn new() -> Self {
+        Self {
+            proxies_t: HashMap::new(),
+            listeners: HashMap::new(),
+        }
+    }
+
+    fn add_proxy_t(&mut self, proxy_t: Box<dyn ProxyT>, listener: Box<dyn Listener>) {
+        let proxy_id = {
+            let proxy = proxy_t.upcast_ref();
+            proxy.id()
+        };
+
+        self.proxies_t.insert(proxy_id, proxy_t);
+        self.listeners.insert(proxy_id, listener);
+    }
+}
 
 fn monitor(remote: Option<String>) -> Result<()> {
     let main_loop = pw::MainLoop::new()?;
@@ -64,8 +88,7 @@ fn monitor(remote: Option<String>) -> Result<()> {
     let registry_weak = Arc::downgrade(&registry);
 
     // Proxies and their listeners need to stay alive so store them here
-    let proxies: RefCell<Vec<Box<dyn ProxyT>>> = RefCell::new(Vec::new());
-    let listeners: RefCell<Vec<Box<dyn Listener>>> = RefCell::new(Vec::new());
+    let proxies = RefCell::new(Proxies::new());
 
     let _registry_listener = registry
         .add_listener_local()
@@ -84,8 +107,9 @@ fn monitor(remote: Option<String>) -> Result<()> {
                             })
                             .register();
 
-                        proxies.borrow_mut().push(Box::new(node));
-                        listeners.borrow_mut().push(Box::new(obj_listener));
+                        proxies
+                            .borrow_mut()
+                            .add_proxy_t(Box::new(node), Box::new(obj_listener));
                     }
                     ObjectType::Port => {
                         let port: Port = registry.bind(&obj).unwrap();
@@ -99,8 +123,9 @@ fn monitor(remote: Option<String>) -> Result<()> {
                             })
                             .register();
 
-                        proxies.borrow_mut().push(Box::new(port));
-                        listeners.borrow_mut().push(Box::new(obj_listener));
+                        proxies
+                            .borrow_mut()
+                            .add_proxy_t(Box::new(port), Box::new(obj_listener));
                     }
                     ObjectType::Module
                     | ObjectType::Device
