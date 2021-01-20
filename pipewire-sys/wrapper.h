f69660a9ecb8c070707af3d33cf3003419b9c297
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2021-01-20 11:48:02 +0100

    pw-sys: generate bindings for extensions as well
    Needed to generate some PW_VERSION constants.

--- pipewire-sys/wrapper.h
+++ pipewire-sys/wrapper.h
@@ -1,1 +1,6 @@
 #include <pipewire/pipewire.h>
+#include <pipewire/extensions/client-node.h>
+#include <pipewire/extensions/metadata.h>
+#include <pipewire/extensions/profiler.h>
+#include <pipewire/extensions/protocol-native.h>
+#include <pipewire/extensions/session-manager.h>
